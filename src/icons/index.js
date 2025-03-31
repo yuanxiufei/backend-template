305c09708efa9b1c62256e860d531079d499c997
ReginYuan <2496943860@qq.com>
2025-03-31 10:20:59 +0800

    feat: 修改项目配置

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,19 +1,19 @@
+// @/icons/index.js
 import SvgIcon from '@/components/SvgIcon'
 
-// 使用 import.meta.glob 动态导入 SVG 文件
-const svgRequire = import.meta.glob('./svg/*.svg', { eager: true })
+const installIcons = app => {
+  // Make sure you're passing the app instance to the component registration
+  const svgRequire = import.meta.glob('./svg/*.svg', { eager: true })
 
-// 遍历所有 SVG 文件，动态注册为 Vue 组件
-Object.keys(svgRequire).forEach(key => {
-  // 提取文件名并直接使用（不转为大驼峰）
-  const componentName = key.split('/').pop().replace('.svg', '')
+  Object.keys(svgRequire).forEach(key => {
+    const componentName = key.split('/').pop().replace('.svg', '')
 
-  // 注册每个 SVG 文件作为 Vue 组件
-  // eslint-disable-next-line no-undef
-  app.component(componentName, svgRequire[key].default)
-})
+    // Register the component globally
+    app.component(componentName, svgRequire[key].default)
+  })
 
-export default app => {
-  // 注册 SvgIcon 组件
+  // Register the SvgIcon component globally
   app.component('svg-icon', SvgIcon)
 }
+
+export default installIcons
